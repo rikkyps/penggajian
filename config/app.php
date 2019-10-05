d978795d5b9a4433aca76bd863efc8a4d23fbf2c
rikkyps <rikky.putra14@gmail.com>
2019-10-05 10:01:52 +0700

    mengganti resource view karyawan

--- config/app.php
+++ config/app.php
@@ -236,7 +236,6 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
-
     ],
 
 ];
